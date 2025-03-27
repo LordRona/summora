5318f9c340db4ea2d912dc1093720b6a5b0a193b
LordRona <ronardsauh@gmail.com>
2025-03-27 15:23:48 +0100

    Second commit message

---
+++ Projects/summora/summora/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata: Metadata = {
+  title: "Summora - AI-powered PDF summarization",
+  description: 
+  "Summora is an app for summarizing web PDFs. Save hours of reading time. Transform PDFs into clear, accurate summaries in seconds with our advance AI technology",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${fontSans.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
